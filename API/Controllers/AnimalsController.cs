5809cc300b374b39f537f9b8d01e0a364b9d78f1
elenabor2003@gmail.com <elenabor2003@gmail.com>
2022-11-03 14:27:22 +0300

    Добавьте файлы проекта.

---
+++ API/Controllers/AnimalsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API.Models;
+
+namespace API.Controllers
+{
+    public class AnimalsController : ApiController
+    {
+        private Borisova_pr2Entities db = new Borisova_pr2Entities();
+
+        // GET: api/Animals
+        [ResponseType(typeof(List<ModelAnimals>))]
+        public IHttpActionResult GetAnimals()
+        {
+            return Ok(db.Animals.ToList().ConvertAll(x => new ModelAnimals(x)));
+        }
+
+        // GET: api/Animals/5
+        [ResponseType(typeof(Animals))]
+        public IHttpActionResult GetAnimals(int id)
+        {
+            Animals animals = db.Animals.Find(id);
+            if (animals == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(animals);
+        }
+
+        // PUT: api/Animals/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAnimals(int id, Animals animals)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != animals.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(animals).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnimalsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Animals
+        [ResponseType(typeof(Animals))]
+        public IHttpActionResult PostAnimals(Animals animals)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Animals.Add(animals);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = animals.ID }, animals);
+        }
+
+        // DELETE: api/Animals/5
+        [ResponseType(typeof(Animals))]
+        public IHttpActionResult DeleteAnimals(int id)
+        {
+            Animals animals = db.Animals.Find(id);
+            if (animals == null)
+            {
+                return NotFound();
+            }
+
+            db.Animals.Remove(animals);
+            db.SaveChanges();
+
+            return Ok(animals);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AnimalsExists(int id)
+        {
+            return db.Animals.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ API/Models/ModelAnimals.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class ModelAnimals
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Kingdom { get; set; }
+        public string Type { get; set; }
+        public string Class { get; set; }
+        public string Detachment { get; set; }
+        public string Family { get; set; }
+        public string Image { get; set; }
+
+        public ModelAnimals (Animals animals)
+        {
+            ID = animals.ID;
+            Title = animals.Title;
+            Kingdom = animals.Kingdom;
+            Type = animals.Type;
+            Class = animals.Class;
+            Detachment = animals.Detachment;
+            Family = animals.Family;
+            Image = animals.Image;
+        }
+
+    }
+}
